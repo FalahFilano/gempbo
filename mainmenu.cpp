4d4519ce9be128430b91294d4f59f5f8eeacbe21
ryoookaze <32873309+ryoookaze@users.noreply.github.com>
2017-12-16 18:05:05 +0700

    Changes

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -1,71 +1,11 @@
-#include "mainmenu.h"
+#include "MainMenu.h"
 
+MainMenu::MainMenu()
+{
+    //ctor
+}
 
-mainmenu::~mainmenu()
+MainMenu::~MainMenu()
 {
     //dtor
 }
-
-
-mainmenu::mainmenu(float width, float height)
-{
-    if(!font.loadFromFile("Gotham-Medium.ots"))
-            imageSprite.setTexture(texture);
-
-	text.setFont(font);
-	text.setColor(sf::Color::Cyan);
-	text.setString(" ");
-    text1.setFont(font);
-    text1.setCharacterSize(15);
-	text1.setColor(sf::Color::White);
-	text1.setPosition(420,650);
-
-	menu[0].setFont(font);
-	menu[0].setColor(sf::Color::Cyan);
-	menu[0].setString("Play The Game");
-    menu[0].setPosition(sf::Vector2f(width/2,height/(MAX_NUMBER_OF_ITEMS+1)*1));
-
-    menu[1].setFont(font);
-    menu[1].setColor(sf::Color::White);
-    menu[1].setString("Exit To Desktop");
-    menu[1].setPosition(sf::Vector2f(width/2,height/(MAX_NUMBER_OF_ITEMS+1)*2));
-
-    selectedItemIndex = 0;
-
-}
-
-void mainmenu::layout(sf::RenderWindow& window)
-{
-    window.draw(text);
-    window.draw(text1);
-	for (int i = 0; i < MAX_NUMBER_OF_ITEMS; i++)
-	{
-        menu[i].setCharacterSize(20);
-		window.draw(menu[i]);
-	}
-}
-
-void mainmenu::gerakatas()
-{
-    if (selectedItemIndex - 1 >= 0)
-	{
-		menu[selectedItemIndex].setColor(sf::Color::White);
-		selectedItemIndex--;
-		menu[selectedItemIndex].setColor(sf::Color::Cyan);
-	}
-}
-
-void mainmenu::gerakbawah()
-{
-    if (selectedItemIndex + 1 < MAX_NUMBER_OF_ITEMS)
-	{
-		menu[selectedItemIndex].setColor(sf::Color::White);
-		selectedItemIndex++;
-		menu[selectedItemIndex].setColor(sf::Color::Cyan);
-	}
-}
-int mainmenu::GetSelectedItem()
-{
-    return this->selectedItemIndex;
-}
-
--- mainmenu.h
+++ mainmenu.h
@@ -1,29 +1,16 @@
 #ifndef MAINMENU_H
 #define MAINMENU_H
 
-#include <SFML/Graphics.hpp>
-#define MAX_NUMBER_OF_ITEMS 3
 
-class mainmenu
+class MainMenu
 {
     public:
-        mainmenu(float width, float height);
-        virtual ~mainmenu();
+        MainMenu();
+        virtual ~MainMenu();
 
-        void layout(sf::RenderWindow &window);
-        void gerakatas();
-        void gerakbawah();
-        int GetSelectedItem ();
     protected:
 
     private:
-        int selectedItemIndex;
-        sf::Font font;
-        sf::Texture texture;
-        sf::Text menu[MAX_NUMBER_OF_ITEMS];
-        sf::Text text;
-        sf::Text text1;
-        sf::Sprite imageSprite;
 };
 
 #endif // MAINMENU_H
